197bc573f83a62454722cabd2daee759e86f1a04
Reynault Sies <reynault.sies@gmail.com>
2021-09-07 15:31:00 +0200

    fix a little bug

--- src/pages/play-in-game.tsx
+++ src/pages/play-in-game.tsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import { ChallengesContext } from '../contexts/challenges/challenges-context';
 import { SimpleGame } from '../components/game/simple-game';
 import { GameProvider } from '../contexts/games/game-context';
+import { Routes } from '../shared/constants/routes';
 
 /**
  * In game page
@@ -13,6 +14,9 @@ export const PlayInGame: React.FunctionComponent = () => {
   const { challengeId }: any = useParams();
   // fetching wanted challenge
   const challenge = getOne(challengeId);
+  if (!challenge) {
+    return <Redirect to={Routes.PLAY_SELECTION} />;
+  }
   return (
     // using game provider with challenge to type
     <GameProvider initialText={challenge.text}>
